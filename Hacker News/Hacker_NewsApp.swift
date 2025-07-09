dd0a10f8fe640c10972a6dad0a180388b27efcc8
Marcos Sanchez <marsanmar6291@gmail.com>
2025-07-09 15:25:05 -0600

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Marcos Sanchez Martinez on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
